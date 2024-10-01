f8ff5869f77d2927e179a6fd03a6f79b664cef91
CodeLaMat <eyvaz.alishov@gmail.com>
2024-10-01 12:14:51 +0300

    Refactor code to update home page layout and styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -28,65 +28,65 @@ export default function Home() {
             <button type="button">Browse recipes</button>
           </Link>
         </div>
-        <div className={classes.home_content_container}>
-          <div>
-            <h2>Looking for some recipes?</h2>
+      </div>
+      <div className={classes.home_content_container}>
+        <div>
+          <h2>Looking for some recipes?</h2>
+        </div>
+        <div className={classes.home_content_container_boxes}>
+          <div className={classes.home_content_box}>
+            <div className={classes.content}>
+              <div className={classes.front}>
+                <h3 className={classes.title}>Browse recipes</h3>
+                <p className={classes.subtitle}>
+                  Find your favourites in this collection. You can search
+                  receipes based on the name or country
+                </p>
+              </div>
+              <div className={classes.back}>
+                <Link to="/recipes" className={classes.description}>
+                  All recipes
+                </Link>
+              </div>
+            </div>
           </div>
-          <div className={classes.home_content_container_boxes}>
+          <div>
+            {" "}
             <div className={classes.home_content_box}>
               <div className={classes.content}>
                 <div className={classes.front}>
-                  <h3 className={classes.title}>Browse recipes</h3>
+                  <h3 className={classes.title}>Add recipe</h3>
                   <p className={classes.subtitle}>
-                    Find your favourites in this collection. You can search
-                    receipes based on the name or country
+                    Recipe from your country is missing? No worries, add one!
                   </p>
                 </div>
                 <div className={classes.back}>
-                  <Link to="/recipes" className={classes.description}>
-                    All recipes
+                  <Link to="/add_recipe" className={classes.description}>
+                    Add recipe
                   </Link>
                 </div>
               </div>
             </div>
-            <div>
-              {" "}
-              <div className={classes.home_content_box}>
-                <div className={classes.content}>
-                  <div className={classes.front}>
-                    <h3 className={classes.title}>Add recipe</h3>
-                    <p className={classes.subtitle}>
-                      Recipe from your country is missing? No worries, add one!
-                    </p>
-                  </div>
-                  <div className={classes.back}>
-                    <Link to="/add_recipe" className={classes.description}>
-                      Add recipe
-                    </Link>
-                  </div>
+          </div>
+          <div>
+            {" "}
+            <div className={classes.home_content_box}>
+              <div className={classes.content}>
+                <div className={classes.front}>
+                  <h3 className={classes.title}>
+                    Want to know more about our project?
+                  </h3>
+                  <p className={classes.subtitle}>
+                    Visit our programme homepage
+                  </p>
                 </div>
-              </div>
-            </div>
-            <div>
-              {" "}
-              <div className={classes.home_content_box}>
-                <div className={classes.content}>
-                  <div className={classes.front}>
-                    <h3 className={classes.title}>
-                      Want to know more about our project?
-                    </h3>
-                    <p className={classes.subtitle}>
-                      Visit our programme homepage
-                    </p>
-                  </div>
-                  <div className={classes.back}>
-                    <ExternalLink
-                      href="https://www.bc.fi/"
-                      className={classes.description}
-                    >
-                      Business College Helsinki homepage
-                    </ExternalLink>
-                  </div>
+                <div className={classes.back}>
+                  <ExternalLink
+                    href="https://www.bc.fi/"
+                    className={classes.description}
+                  >
+                    Business College Helsinki homepage
+                  </ExternalLink>
                 </div>
               </div>
             </div>
